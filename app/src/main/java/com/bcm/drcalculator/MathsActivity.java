f8d12f72e844d64cd27e3d28d548278cc5fb247d
evan <evansullivan74@gmail.com>
2023-06-18 16:09:08 +0100

    completed the temp activity UI

--- app/src/main/java/com/bcm/drcalculator/MathsActivity.java
+++ app/src/main/java/com/bcm/drcalculator/MathsActivity.java
@@ -8,10 +8,16 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MathsActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private Button toTemp;
+    private Button toDist;
+    private Button toMatrix;
+    private Button toRNG;
+    private Button toBin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,12 @@ public class MathsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maths);
 
         toolbar = findViewById(R.id.toolbar);
+        toTemp = findViewById(R.id.toTemp);
+        toDist = findViewById(R.id.toDistance);
+        toMatrix = findViewById(R.id.toMatrix);
+        toRNG = findViewById(R.id.toRNG);
+        toBin = findViewById(R.id.toBinary);
+
 
 
         toolbar.setTitle("Maths");
@@ -36,6 +48,46 @@ public class MathsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MathsActivity.this, menuActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+            }
+        });
+
+        toTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MathsActivity.this, TempActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+            }
+        });
+
+        toDist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MathsActivity.this, DistActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+            }
+        });
+
+        toMatrix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MathsActivity.this, MatrixActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+            }
+        });
+
+        toRNG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MathsActivity.this, randomNumberActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+            }
+        });
+
+        toBin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MathsActivity.this, BinaryActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
 
             }
         });
---
+++ app/src/main/java/com/bcm/drcalculator/TempActivity.java
@@ -0,0 +1,33 @@
+package com.bcm.drcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TempActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+        toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setTitle("Temperature");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TempActivity.this, MathsActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+            }
+        });
+    }
+}
